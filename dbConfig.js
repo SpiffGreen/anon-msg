ed2a82022a4b568f3995bcf33de95d91cb6cd1cc
SpiffGreen <spiffjekeygreen@gmail.com>
2021-07-21 03:26:03 +0100

    Fixed mongodb index ttl issue

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DB_URI_DEV, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
     .then(() => console.dir("Connected to Database"))
     .catch((err) => console.warn(err));
